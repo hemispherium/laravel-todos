9af94690894e22141e8ba6892093ae60f4b0c20e
hemispherium <nkmr357@gmail.com>
2021-07-02 22:47:57 +0900

    finish chap. (3)

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// https://qiita.com/tamakiiii/items/e71040173fa0a1fcad83
+Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
