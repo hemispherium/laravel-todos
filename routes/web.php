1d6564ca5375a94703e889208036b997ab0b9786
hemispherium <nkmr357@gmail.com>
2021-07-04 16:43:56 +0900

    finish chap. (5)

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 // https://qiita.com/tamakiiii/items/e71040173fa0a1fcad83
+
 Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
+
+Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
